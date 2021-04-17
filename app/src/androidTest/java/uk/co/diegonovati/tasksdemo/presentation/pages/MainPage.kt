4345dd354db680838adb684d6ad20b9cfac2df07
diegonovati <diego.novati@itmms.co.uk>
2021-04-17 21:08:36 +0100

    Updated check

--- app/src/androidTest/java/uk/co/diegonovati/tasksdemo/presentation/pages/MainPage.kt
+++ app/src/androidTest/java/uk/co/diegonovati/tasksdemo/presentation/pages/MainPage.kt
@@ -1,36 +1,43 @@
 package uk.co.diegonovati.tasksdemo.presentation.pages
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import org.hamcrest.CoreMatchers.allOf
 import uk.co.diegonovati.tasksdemo.R
 import uk.co.diegonovati.tasksdemo.data.models.ConnectionStatus
 import uk.co.diegonovati.tasksdemo.presentation.activity.MainActivity
 
-class MainPage: Page(withRootId(R.id.mainActivity)) {
+class MainPage : Page(withRootId(R.id.mainActivity)) {
 
-    fun setDeviceConnectionStatus(mainActivity: MainActivity, connectionStatus: ConnectionStatus): MainPage {
+    fun setDeviceConnectionStatus(
+        mainActivity: MainActivity,
+        connectionStatus: ConnectionStatus
+    ): MainPage {
         mainActivity.getViewModel().updateInternetConnectionStatus(connectionStatus)
         return this
     }
 
     fun checkListIsVisible(): MainPage {
-        onView(allOf(withText("Take the rubbish out"))).check(ViewAssertions.matches(isDisplayed()))
+        onView(allOf(withText("Take the rubbish out"))).check(matches(isDisplayed()))
         return this
     }
 
     fun ensureConnectionStatusIsOnline(): MainPage {
-        Thread.sleep(500)
-        onView(withId(R.id.deviceOnline)).check(matches(ViewMatchers.isDisplayed()))
+        waitUntil {
+            !throwsException {
+                onView(withId(R.id.deviceOnline)).check(matches(isDisplayed()))
+            }
+        }
         return this
     }
 
     fun ensureConnectionStatusIsOffline(): MainPage {
-        Thread.sleep(500)
-        onView(withId(R.id.deviceOffline)).check(matches(ViewMatchers.isDisplayed()))
+        waitUntil {
+            !throwsException {
+                onView(withId(R.id.deviceOffline)).check(matches(isDisplayed()))
+            }
+        }
         return this
     }
 }
